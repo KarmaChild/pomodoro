236ea52d363a09e8f93e700c56ee05e006ac5872
adith <adm115@mail.usask.ca>
2022-09-24 19:39:02 -0600

    commit 3

--- src/pomodoro.js
+++ src/pomodoro.js
@@ -3,11 +3,32 @@ import React,{useState,useEffect} from "react";
 
 const Pomodoro = () => {
 
-    const [minutes, setMinutes] = useState(25);
-    const [seconds, setSeconds] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const [seconds, setSeconds] = useState(5);
     const [displayMessage, setDisplayMessage] = useState(false);
 
-    useEffect(() =>{},[seconds]);
+    useEffect(() =>{
+        let interval = setInterval(()=>{
+            clearInterval(interval);
+
+            if(seconds === 0){
+                if (minutes !== 0){
+                    setSeconds(59);
+                    setMinutes(minutes-1);
+                } else{
+                    let minutes = displayMessage ? 24 : 4;
+                    let seconds = 59;
+
+                    setSeconds(seconds);
+                    setMinutes(minutes);
+                    setDisplayMessage(!displayMessage);
+                }
+            }else {
+                setSeconds(seconds-1);
+            }
+
+        },1000)
+    },[seconds]);
 
 
     const timerMinutes = minutes < 10 ? `0${minutes}` : minutes;
